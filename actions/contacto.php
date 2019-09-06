46f7bc684c19358efb61e9d0c3523580a7102e3c
Xeron081495 <germang08@hotmail.com>
2019-09-05 22:31:38 -0300

    correcciones finales

--- actions/contacto.php
+++ actions/contacto.php
@@ -16,7 +16,7 @@ $mensaje   = '
 	<hr>
 	
 	<p><b>Nombre</b>: '.$nombre.'</p>
-	<p><b>Telefono</b>: '.$telefono.'</p>
+	<p><b>Teléfono</b>: '.$telefono.'</p>
 	<p><b>Correo</b>: '.$correo.'</p>
 	<p><b>Empresa</b>: '.$empresa.'</p>
 	
