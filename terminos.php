839620866525c4eca48af977bdd05ab9d9b58e79
Xeron081495 <germang08@hotmail.com>
2020-07-27 17:25:41 -0300

    agregar terminos y condiciones

---
+++ terminos.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Estrategia Sur es una Agencia Comercial con la que nos dedicamos a la representación oficial de marcas líderes en el mercado de la construcción sustentable.">
+    <meta name="keywords" content="Acero Latina, Paneles Aislantes, Frio Latina, Bervill, representación comercial, construcción sustentable">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sección | Estrategia Sur | Paneles Aislantes | Bahía Blanca</title>
+
+    <!-- Icono -->
+    <link rel="icon" type="image/png" href="img/logo.png" />
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,300i,400,500,700,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,300,300i,400,500,700,900&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="lib/fontawesome/css/all.css" type="text/css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/barfiller.css" type="text/css">
+    <link rel="stylesheet" href="css/header.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/sucursales.css" type="text/css">
+    <link rel="stylesheet" href="css/index.css" type="text/css">
+
+    <!-- scripts -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script defer src="lib/fontawesome/js/all.js"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-147471360-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-147471360-1');
+    </script>
+</head>
+
+<body id="inicio">
+
+    <!-- Header Section Begin -->
+    <?php include('bloques/header.php'); ?>
+    <!-- Header End -->
+    
+    <!-- TITULO -->
+    <section class="titulo">
+        <h1>Panel FOILROOF Trapezoidal o Sinusoidal</h1>
+        <a href="index">
+            <img class="marca d-none" alt="Acerolatina" title="Acerolatina" src="img/logos_empresas/acerolatina.jpg"/>
+        </a>
+    </section>
+    <!-- TITULO ENDS-->
+
+    <section class="pr-0 pl-0 sucursales col-12 float-left clearfix nosotros-section">
+      <div class="mx-auto col-12 col-sm-10 col-xl-9 p-0 m-0 pl-3 pr-3 pl-md-0 pr-md-0 pt-5 container">
+        
+        <div class="col-12 p-0 float-left mb-5">
+          <h2 class="text-left col-12 mt-0 pt-3 pb-0 mb-4 pl-0 pl-md-2 titulo-departamento">PRECIO : USD 17,00 x m²</h2>
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0"><i class="fas fa-check mr-4"></i>El precio no incluye IVA.</p>
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0"><i class="fas fa-check mr-4"></i>No incluye flete.</p>
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0"><i class="fas fa-check mr-4"></i>No incluye accesorios de fijación, ni montaje.</p>
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0"><i class="fas fa-check mr-4"></i>Valor del dólar equivale al tipo vendedor del Banco de la Nación Argentina, del día de formalizada la operación.</p>
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0"><i class="fas fa-check mr-4"></i>Forma de pago: Contado anticipado. <span class="text-danger">(Consultar por otras opciones).</span></p>
+        </div>
+
+        <div class="col-12 p-0 float-left mb-1">
+          <h2 class="text-left col-12 mt-0 pt-3 pb-0 mb-4 pl-0 pl-md-2 titulo-departamento">Especificaciones técnicas</h2>
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0"><i class="fas fa-check mr-4"></i>Cara exterior de acero galvanizado de 0,5 mm de espesor.</p>
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0"><i class="fas fa-check mr-4"></i>Aislación Poliuretano de 10 mm de espesor. (Consultar precio por otros espesores).</p>
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0"><i class="fas fa-check mr-4"></i>Cara interior Foil de polietileno Blanco de 0,08 mm de espesor.</p>
+        </div>
+
+        <div class="boton-contacto mb-4 text-right">
+          <a type="button" href="#contacto" class="btn btn-info text-light">Contacto</a>
+        </div>
+
+
+        <div class="col-12 p-0 float-left">
+          <h2 class="text-left col-12 mt-0 pt-3 pb-0 mb-4 pl-0 pl-md-2 titulo-departamento">Fabricación y logística</h2>
+          <h3 class="col-12 titulo-departamento mb-4 pl-0 pl-md-2 ">Plazo de entrega: </h3>
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0"><i class="fas fa-check mr-4"></i>Para compras mayores a 400 m², el plazo será de 15 días.</p>
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0"><i class="fas fa-check mr-4"></i>Para compras menores a 400 m², el plazo de entrega dependerá de fecha de producción.</p>
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0">Los espesores indicados son nominales.</p>
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0">La mercadería se entrega puesta sobre camión, en las plantas industriales de Acerolatina sita en Navarra s/n entre Barcelona y Galicia (1748). Gral. Rodriguez, Buenos Aires, Argentina; o en Carril Rodriguez Peña 1009. Gral. Gutierrez. Maipú, Mendoza, Argentina. A conveniencia del comprador.</p>
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0">La mercadería deberá retirarse de la planta en un plazo máximo de 7 días corridos a partir de la fecha que se le informe su disponibilidad.</p>    
+        </div>
+
+        <div class="col-12 p-0 float-left mt-4">
+          <p class=" departamento ml-0 ml-md-3 col-12 pl-0 text-danger"><b>Para solicitar una cotización, comuníquese o envíe el formulario de contacto adjunto.</b></p>    
+        </div>
+
+        <div class="boton-contacto mb-4 text-right">
+          <a type="button" href="#contacto" class="btn btn-info text-light">Contacto</a>
+        </div>
+      </div>
+    </section>
+    
+
+    <div class="clearfix"></div>
+    
+    <!-- CONTACTO Y FOOTER BEGIN -->
+    <?php include('bloques/footer.php'); ?>
+    <!-- CONTACTO Y FOOTER END -->
+
+    <!-- Js Plugins -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/circle-progress.min.js"></script>
+    <script src="js/jquery.barfiller.js"></script>
+    <script src="js/main.js"></script>
+    <script>
+        const param = getURLParameter('t');  
+        if(param!=null){
+            document.getElementById("mensaje-contacto").innerHTML = param;
+        }
+    </script>
+</body>
+</html>
